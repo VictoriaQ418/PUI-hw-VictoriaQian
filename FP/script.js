873cd2d73041de911b172b9e107d660d8d361661
VictoriaQ418 <122949607+VictoriaQ418@users.noreply.github.com>
2023-05-02 02:40:43 -0400

    FP

---
+++ FP/script.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+    var typed = new Typed('.typing', {
+      strings: ['Student', 'Designer', 'Blogger'],
+      typeSpeed: 100,
+      backSpeed: 60,
+      loop: true
+    });
+  });
+  
